126fb6e3400887e3613d1c020c497e04f8faeec2
mat10599 <78880807+mat10599@users.noreply.github.com>
2023-10-23 14:16:13 +0200

    linter

--- src/modelling/preprocessing.py
+++ src/modelling/preprocessing.py
@@ -35,18 +35,22 @@ def preprocessing(
     if not training:
         # check if scaler and label_encoder are provided
         if scaler is None or label_encoder is None:
-            raise ValueError("Please provide scaler and label_encoder objects in predicting mode")
+            raise ValueError(
+                "Please provide scaler and label_encoder objects in predicting mode")
     if training:
         df["age"] = df["Rings"] + 1.5
         df = df.drop("Rings", axis=1)
 
         label_encoder = LabelEncoder()
         numerical_encoders = StandardScaler()
-        df[NUMERICAL_COLS] = numerical_encoders.fit_transform(df[NUMERICAL_COLS])
+        df[NUMERICAL_COLS] = numerical_encoders.fit_transform(
+            df[NUMERICAL_COLS])
         df["Sex"] = label_encoder.fit_transform(df["Sex"])
 
-        save_pickle(numerical_encoders, os.path.join(LOCAL_OBJECTS_PATH, "numerical_encoders.pkl"))
-        save_pickle(label_encoder, os.path.join(LOCAL_OBJECTS_PATH, "label_encoder.pkl"))
+        save_pickle(numerical_encoders, os.path.join(
+            LOCAL_OBJECTS_PATH, "numerical_encoders.pkl"))
+        save_pickle(label_encoder, os.path.join(
+            LOCAL_OBJECTS_PATH, "label_encoder.pkl"))
 
     else:
         df[[NUMERICAL_COLS]] = scaler.transform(df[[NUMERICAL_COLS]])
